9c81a814a3c823031a4907d306bcbcfd4d910a09
Leonardo Nogueira <nogueirahy@gmail.com>
2022-07-17 14:48:58 -0400

    feat: create product entity

---
+++ domain/entities/product_entity.go
@@ -0,0 +1,19 @@
+package entities
+
+type ProductEntity struct {
+	Id                   string  `json:"productId"`
+	ProductName          string  `json:"productName"`
+	PromotionalTag       string  `json:"promotionalTag"`
+	ClusterTag           string  `json:"clusterTag"`
+	PriceWithoutDiscount float64 `json:"priceWithoutDiscount"`
+	Price                float64 `json:"price"`
+	AvailableQuantity    int     `json:"availableQuantity"`
+	IsAvailable          bool    `json:"isAvailable"`
+	ImageUrl             string  `json:"imageUrl"`
+	MeasurementUnit      string  `json:"measurementUnit"`
+	UnitMultiplier       float64 `json:"unitMultiplier"`
+}
+
+func (p *ProductEntity) SomeBusinessLogic() bool {
+	return p.AvailableQuantity > 0
+}

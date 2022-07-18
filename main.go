b01a52d4085561ac94e6ce93ee8a67090a82b40d
Leonardo Nogueira <nogueirahy@gmail.com>
2022-07-17 23:56:04 -0400

    feat: add Dependency injection to ProductSearch

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	vtexapi "github.com/nogueirahy/go-clean-architecture/infra/vtex_api/product"
+	presentation "github.com/nogueirahy/go-clean-architecture/presentation/presenters/product/product_search"
+	service "github.com/nogueirahy/go-clean-architecture/service/usecases/product"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	vtexApi := vtexapi.VtexApi{}
+	productSearchService := service.ProductSearchUseCase{VtexApi: &vtexApi}
+	productSearchController := presentation.ProductSearchPresenter{ProductSearchUseCase: &productSearchService}
+
+	productEntity := productSearchController.Execute()
+
+	fmt.Println(productEntity.ProductName)
+	fmt.Println(productEntity.SomeBusinessLogic())
 }
